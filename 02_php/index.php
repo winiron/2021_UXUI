10e9b3a5dbd7f86660ed0fbb9128e0da43b42693
winiorn <yamg1015@gmail.com>
2021-09-06 16:10:33 +0900

    20210906_update

---
+++ 02_php/index.php
@@ -0,0 +1,7 @@
+<?include "header.php"?>
+    <div class="visual">visual</div>
+    <div class="contents clearfix">
+        <section class="section">section</section>
+        <aside class="aside">aside</aside>
+    </div>
+<?include "footer.php"?>
---
+++ 02_php/sub01.php
@@ -0,0 +1,5 @@
+<?include "header.php"?>
+<div class="subVisual">subVisual_01</div>
+<div class="contents"></div>
+
+<?include "footer.php"?>
