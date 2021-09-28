daa06519fcb89c7987488cff01e57caa12ab7f51
winiron <yamg1015@gmail.com>
2021-09-28 10:39:18 +0900

    2021-09-28-원격수업

--- 01html/01_javascript/2021_09_27_scroll/script.js
+++ 01html/01_javascript/2021_09_27_scroll/script.js
@@ -1,14 +1,19 @@
+
+const menu = document.querySelector(".menu"); //전역변수
+
 window.addEventListener("scroll",function(){
     let scroll = window.pageYOffset;
     let y = document.querySelector(".scroll");
-    const menu = document.querySelector(".menu");
+    //const menu = document.querySelector(".menu");//지역변수
     y.innerHTML = scroll;
 
-    if (scroll > 300) {
-        menu.style.position = "fixed";
-        document.querySelector(".menu").style.top = "0";
+    if (scroll > 500) {
+        // menu.style.position = "fixed";
+        // document.querySelector(".menu").style.top = "0";
+        menu.classList.add("fixed") // jQuery : addClass("fixed")
     } else {
-        menu.style.position = "absolute";
-        menu.style.top = "20px";
+        // menu.style.position = "absolute";
+        // menu.style.top = "20px";
+        menu.classList.remove("fixed") // jQuery : removeClass("fixed")
     }
 });
