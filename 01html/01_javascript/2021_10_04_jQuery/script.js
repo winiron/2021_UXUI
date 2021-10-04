a97a5ffa54a52329d777cc2739d900b6b99ac5bf
winiron <yamg1015@gmail.com>
2021-10-04 13:38:50 +0900

    2021-10-04-UPDATE

--- 01html/01_javascript/2021_10_04_jQuery/script.js
+++ 01html/01_javascript/2021_10_04_jQuery/script.js
@@ -1,4 +1,14 @@
 //btn click  ⇨ "p"tag .hide() 
-$(".btn").click(function(){
-    $("#text > p").hide();
-});
+
+//jQuery
+// $(".btn").click(function(){
+//     $("#text > p").hide();
+// });
+
+//javaScript
+const btn = document.querySelector(".btn");
+const pTag = document.querySelector("p");
+
+btn.onclick = function(){
+    pTag.style.display="none";
+};
