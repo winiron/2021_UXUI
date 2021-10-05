1e2c35dddc2d623aad0f5c4c69aa9726251ae30b
winiron <yamg1015@gmail.com>
2021-10-05 10:36:58 +0900

    2021-10-05-UPDATE

--- 01html/01_javascript/2021_10_04_jQuery/modal.js
+++ 01html/01_javascript/2021_10_04_jQuery/modal.js
@@ -6,10 +6,12 @@ $("body").append(`
 
 //jQuery 문법
 $("a.modal").click(function(event) { //$("선택").on("click", function(){})
-    event.preventDefault();        // a tag href 기본 이벤트를 막아줌. 
-    $(".glayLayer").fadeIn();
+    event.preventDefault();         // a tag href 기본 이벤트를 막아줌. 
+    $(".glayLayer").fadeIn();       // $(selector).fadeIn(speed,easeing, callback) 
+    
     // let aHref = $(a.modal).attr("href");
-    let aHref = $(this).attr("href"); // $(this) ≒ a.modal 
+    // $(this) ≒ a.modal 
+    let aHref = $(this).attr("href"); // $(selector).attr(attribute) 값을 가져옴 (get)
     $(".overLayer").fadeIn().html(`<img src="${aHref}">`);
     //document.querySelector("").innerHTML = `<img src="${aHref}">` // javaScript 형식
 });
