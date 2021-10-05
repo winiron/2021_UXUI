0e6a07c23bb052fb709505b53ca81746f24d2065
winiron <yamg1015@gmail.com>
2021-10-05 10:08:18 +0900

    2021-10-05-UPDATE

--- 01html/01_javascript/2021_10_04_jQuery/modal.js
+++ 01html/01_javascript/2021_10_04_jQuery/modal.js
@@ -1,10 +1,17 @@
-$("body").append(`<div class="glayLayer"></div><div class="overLayer"></div>`);
+//요소 삽입 (jQuery)
+$("body").append(`
+    <div class="glayLayer"></div>
+    <div class="overLayer"></div>
+`);
 
-$("a.modal").click(function(e){
-    e.preventDefault();        // a tag href 기본 이벤트를 막아줌. 
+//jQuery 문법
+$("a.modal").click(function(event) { //$("선택").on("click", function(){})
+    event.preventDefault();        // a tag href 기본 이벤트를 막아줌. 
     $(".glayLayer").fadeIn();
-    let aHref = $(this).attr("href");
+    // let aHref = $(a.modal).attr("href");
+    let aHref = $(this).attr("href"); // $(this) ≒ a.modal 
     $(".overLayer").fadeIn().html(`<img src="${aHref}">`);
+    //document.querySelector("").innerHTML = `<img src="${aHref}">` // javaScript 형식
 });
 
 $(".glayLayer, .overLayer").click(function() {
