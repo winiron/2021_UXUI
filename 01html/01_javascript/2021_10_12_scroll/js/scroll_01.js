c6fbea42c53e20bf4e5c6ca1b2c645df23e8b16b
winiron <yamg1015@gmail.com>
2021-10-12 12:19:30 +0900

    2021-10-12-UPDATE-AM

--- 01html/01_javascript/2021_10_12_scroll/js/scroll_01.js
+++ 01html/01_javascript/2021_10_12_scroll/js/scroll_01.js
@@ -1,10 +1,8 @@
 const menuPos = $(".nav").offset().top;
+let scrollY = $(window).scrollTop();
+let halfH = $("body").height() / 2;
 // $(".menuPos").html(parseInt(menuPos));
 $(".menuPos").html(`${menuPos}px`);
-
-
-
-let scrollY = $(window).scrollTop();
 $(".scrollY").html(`${scrollY}px`)
 
 $(window).scroll(function(){
@@ -18,5 +16,15 @@ $(window).scroll(function(){
     } else {
         $(".nav").removeClass("fixed");
     }
+    if(scrollY > halfH){
+        $(".top").show()
+    } else {
+        $(".top").hide()
+    }
+});
+
+// 클릭  ⇨ 상단으로 이동
+$(".top").click(function(){
+    $("html, body").animate({scrollTop:0},1000);
 });
 
