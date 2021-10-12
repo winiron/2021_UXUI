ac3f00e2d8352858ec3f97d0c736c4aaa88779ee
winiron <yamg1015@gmail.com>
2021-10-12 12:55:01 +0900

    2021-10-12-UPDATE-AM

--- 01html/01_javascript/2021_10_12_scroll/js/scroll_01.js
+++ 01html/01_javascript/2021_10_12_scroll/js/scroll_01.js
@@ -3,20 +3,34 @@ let scrollY = $(window).scrollTop();
 let halfH = $("body").height() / 2;
 // $(".menuPos").html(parseInt(menuPos));
 $(".menuPos").html(`${menuPos}px`);
-$(".scrollY").html(`${scrollY}px`)
+$(".scrollY").html(`${scrollY}px`);
 
 $(window).scroll(function(){
     //console.log(window.pageYOffset) // javascript scroll value
     //console.log($(window).scrollTop()); // jquery scroll value
     scrollY = $(window).scrollTop();
+    
     $(".scrollY").html(`${scrollY}px`);
 
-    if(menuPos < scrollY){
+    if (menuPos < scrollY) {
         $(".nav").addClass("fixed");
     } else {
         $(".nav").removeClass("fixed");
     }
-    if(scrollY > halfH){
+
+    if ( scrollY > $(".car1").offset().top - 800 ) {
+        $(".car1").addClass("move1")
+    } else {
+        $(".car1").removeClass("move1")
+    }
+
+    if ( scrollY > $(".car2").offset().top - 800 ) {
+        $(".car2").addClass("move2")
+    } else {
+        $(".car2").removeClass("move2")
+    }
+
+    if (scrollY > halfH) {
         $(".top").show()
     } else {
         $(".top").hide()
@@ -25,6 +39,5 @@ $(window).scroll(function(){
 
 // 클릭  ⇨ 상단으로 이동
 $(".top").click(function(){
-    $("html, body").animate({scrollTop:0},1000);
+    $("html,body").animate({scrollTop:0},1000);
 });
-
