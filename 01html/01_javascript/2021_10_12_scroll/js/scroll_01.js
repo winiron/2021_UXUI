d496134daae3703316291c48bf96c16b573fe0e2
winiron <yamg1015@gmail.com>
2021-10-13 16:02:02 +0900

    2021-10-13-Wed-PM-UPDATE

--- 01html/01_javascript/2021_10_12_scroll/js/scroll_01.js
+++ 01html/01_javascript/2021_10_12_scroll/js/scroll_01.js
@@ -1,22 +1,22 @@
-const menuPos = $(".nav").offset().top;
-let scrollY = $(window).scrollTop();
+const menuPos = $(".nav").offset().top;             // 네비게이션(메뉴) "top" 값 ⇨ menuPos  
+let scrollY = $(window).scrollTop();                // 스크롤 위치 값 ⇨ scrollY
 let halfH = $("body").height() / 2;
 // $(".menuPos").html(parseInt(menuPos));
-$(".menuPos").html(`${menuPos}px`);
-$(".scrollY").html(`${scrollY}px`);
+$(".menuPos").html(`${parseInt(menuPos)}px`);       // .menuPos에 값(${menuPos}px)을 입력
+$(".scrollY").html(`${parseInt(scrollY)}px`);      // .scrollY에 값(${scrollY}px)을 입력
 
+//스크롤 이벤트 생성
 $(window).scroll(function() {
-    //console.log(window.pageYOffset) // javascript scroll value
+    //console.log(window.pageYOffset) // javascript scroll value  ∴ window.pageYOffset : 스크롤 발생 시 ⇨ 현재의 스크롤 위치 값 
     //console.log($(window).scrollTop()); // jquery scroll value
-    scrollY = $(window).scrollTop();
-    
-    $(".scrollY").html(`${scrollY}px`);
+    scrollY = $(window).scrollTop(); // 현재의 스크롤 위치 값 ⇨ scrollY
+    $(".scrollY").html(`${parseInt(scrollY)}px`); // .scrollY에 값(${scrollY}px)을 입력
 
-    // 메뉴의 위치(고정) < 스크롤 위치(변동) ⇨ ①
+    // 메뉴의 위치(고정) < 스크롤 위치(변동) ⇨ ①(true) / ②(false)
     if (menuPos < scrollY) {
         $(".nav").addClass("fixed"); // ①
     } else {
-        $(".nav").removeClass("fixed"); 
+        $(".nav").removeClass("fixed"); // ②
     }
 
     if ( scrollY > $(".car1").offset().top - 800 ) {
