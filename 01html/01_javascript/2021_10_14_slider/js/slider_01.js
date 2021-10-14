e7fb562f0f9b8ef7f98d3451e0eac732f66b1b50
winiron <yamg1015@gmail.com>
2021-10-14 12:01:49 +0900

    2021-10-14-Thu-AM-UPLOAD

---
+++ 01html/01_javascript/2021_10_14_slider/js/slider_01.js
@@ -0,0 +1,14 @@
+setInterval(slider,3000);
+
+let current = 0;
+let num = $("#wrap ul li").length;
+// 함수 생성
+function slider(){
+
+    $("#wrap li").eq(current).css("left",0).stop().animate({"left":"-100%"},1000);
+    current++;
+    if (current == num) {
+        current=0;
+    }
+    $("#wrap li").eq(current).css("left","100%").stop().animate({"left":0},1000);
+}
