033c3ebc26439a295b4e41b492d8c45c72c3d568
winiron <yamg1015@gmail.com>
2021-10-18 21:22:02 +0900

    2021-10-18-Mon-AM-UPDATE

--- 01html/2021_10_15_video/js/video_03.js
+++ 01html/2021_10_15_video/js/video_03.js
@@ -2,7 +2,7 @@ const sampleImg = document.querySelector(".sampleImg");
 const totalImg = 122;   // 이미지의 총 개수
 const arrayImg = [];    // 배열 생성 
 let currentImgNum;      // 현재의 위치 이미지 번호
-let progress;
+let progress;           // 0~1사이 값
 let hRatio;
 
 for (let index=0; index<totalImg; index++) {
@@ -10,24 +10,21 @@ for (let index=0; index<totalImg; index++) {
     let elemImg =`images/small/o_intro_small_${1000+index}.jpg`;
     arrayImg.push(elemImg);
 }
-
+//  스크롤 이벤트 발생
 window.addEventListener("scroll",function() {
     progress = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
-    view();
+    view(); // view() 함수 실행
 });
-
+// 리사이즈 이벤트 발생
 window.addEventListener("resize",function() {
     hRatio = window.innerHeight / 1080;
-    view();
+    view(); // view() 함수 실행
 });
 
-
+// view() 함수 생성
 function view(){
     currentImgNum = Math.round(( totalImg - 1 ) * progress);
     console.log(currentImgNum)
     sampleImg.src = arrayImg[currentImgNum];
-
-
     sampleImg.style.transform = `translate3d(-50%,-50%,0) scale(${hRatio})`
-
 }
