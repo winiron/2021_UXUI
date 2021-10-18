92d536e177dc9889c1556cc84511fd22bdd59218
winiron <yamg1015@gmail.com>
2021-10-18 09:48:22 +0900

    2021-10-18-Mon-AM-UPDATE

--- 01html/2021_10_15_video/js/video_03.js
+++ 01html/2021_10_15_video/js/video_03.js
@@ -1,12 +1,12 @@
 const sampleImg = document.querySelector(".sampleImg");
 const totalImg = 122;   // 이미지의 총 개수
-const imgArray = [];    // 배열 생성 
+const arrayImg = [];    // 배열 생성 
 let currentImgNum;      // 현재의 위치 이미지 번호
 
 for (let index=0; index<totalImg; index++) {
     // let imgElem ="images/small/o_intro_small"+(1000 +index)+".jpg";
-    let imgElem =`images/small/o_intro_small_${1000+index}.jpg`;
-    imgArray.push(imgElem);
+    let elemImg =`images/small/o_intro_small_${1000+index}.jpg`;
+    arrayImg.push(elemImg);
 }
 
 window.addEventListener("scroll",function() {
@@ -14,5 +14,5 @@ window.addEventListener("scroll",function() {
     let progress = scrollY / (document.body.offsetHeight - window.innerHeight);
     currentImgNum = Math.round((totalImg -1) * progress);
     console.log(currentImgNum);
-    sampleImg.src = imgArray[currentImgNum];
+    sampleImg.src = arrayImg[currentImgNum];
 });
