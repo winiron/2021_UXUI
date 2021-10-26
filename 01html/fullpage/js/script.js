73c2f3477d782c650ea61f2fb25b633aa4187284
winiron <yamg1015@gmail.com>
2021-10-26 10:29:39 +0900

    2021-10-26-TUE-AM-UPLOAD

---
+++ 01html/fullpage/js/script.js
@@ -0,0 +1,6 @@
+$("#fullpage").fullpage({
+    menu:"#topMenu",
+    anchors:["m1st","m2st","m3st","m4st"],
+    navigation:true,
+    navigationTooltips:['oneSlide','twoSlide', 'threeSlide', 'fourSlide']
+});
