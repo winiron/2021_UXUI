d5474a9417c789b22eeff1769cd6e0f9e3f281af
winiron <yamg1015@gmail.com>
2021-11-02 09:31:20 +0900

    2021-11-02-TUE-AM-UPDATE

--- 01html/2021_11_01_svg_script/js/script.js
+++ 01html/2021_11_01_svg_script/js/script.js
@@ -1,8 +1,8 @@
 window.addEventListener("scroll",function(){
     if ( window.pageYOffset > 300 ) { // window.pageYOffset의 값이 300보다 크면 ⇨ ①
-        document.querySelector("#box").classList.add("on") // ① : #box에 .on삽입
+        document.querySelector("#box").classList.add("on") // ① : #box ⇨ .on삽입
     } else {
-        document.querySelector("#box").classList.remove("on") // ② : #box에 .on제거
+        document.querySelector("#box").classList.remove("on") // ② : #box ⇨ .on제거
     }
 
     let scrollTop = document.querySelector(".scrollTop");
@@ -13,6 +13,6 @@ window.addEventListener("scroll",function(){
     scrollTop.innerHTML = window.pageYOffset;
     // 브라우져 높이 : window.innerHeight
     windowHeight.innerHTML = window.innerHeight; 
-    // 실제스크롤높이 : 현재스크롤 위치 - 브라우져높이 
+    // 스크롤높이 = 현재스크롤 위치 - 브라우져높이 
     scrollHeight.innerHTML = document.body.offsetHeight - window.innerHeight;  
 })
