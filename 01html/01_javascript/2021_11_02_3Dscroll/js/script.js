418f3e2aee75399fec08a4159aaa1d93d82ec9d9
winiron <yamg1015@gmail.com>
2021-11-02 13:44:54 +0900

    2021-11-02-TUE-AM-UPDATE

---
+++ 01html/01_javascript/2021_11_02_3Dscroll/js/script.js
@@ -0,0 +1,3 @@
+// ① document의 높이    : document height     ⇨ document.body.offsetHeight
+// ② 브라우저의 높이    : browser height      ⇨ window.innerHeight
+// ③ 스크롤의 위치(값)  : scroll location     ⇨ window.pageYOffset + scroll event 
